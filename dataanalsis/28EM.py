a82ebc45cb5ded409ce471049ced2780481928ff
jayjiao <918718278@qq.com>
2021-06-13 10:28:03 +0800

    day 45

--- dataanalsis/28EM.py
+++ dataanalsis/28EM.py
@@ -8,9 +8,24 @@ from sklearn.mixture import GaussianMixture
 from sklearn.preprocessing import StandardScaler
 
 # 数据加载，避免中文乱码问题
-data_ori = pd.read_csv('../data/heros.csv', encoding="gb18030")
-features = [u'最大生命',u'生命成长',u'初始生命',u'最大法力', u'法力成长',u'初始法力',u'最高物攻',u'物攻成长',u'初始物攻',u'最大物防',u'物防成长',u'初始物防', u'最大每5秒回血', u'每5秒回血成长', u'初始每5秒回血', u'最大每5秒回蓝', u'每5秒回蓝成长', u'初始每5秒回蓝', u'最大攻速', u'攻击范围']
+data_ori = pd.read_csv('../data/heros.csv', encoding="utf-8")
+features = [u'最大生命',u'生命成长',u'初始生命',u'最大法力', u'法力成长',u'初始法力',u'最高物攻',u'物攻成长',u'初始物攻',u'最大物防',u'物防成长',u'初始物防', u'最大每5秒回血', u'每5秒回血成长', u'初始每5秒回血', u'最大每5秒回蓝', u'每5秒回蓝成长', u'初始每5秒回蓝', u'最大攻速', u'攻击范围',u'英雄']
+# print(data_ori.__dict__)
+print(data_ori[u'主要定位'])
+# print(data_ori.index)
 data = data_ori[features]
+
+# 对英雄属性之间的关系进行可视化分析
+# 设置plt正确显示中文
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False    #用来正常显示负号
+# 用热力图呈现features_mean字段之间的相关性
+corr = data[features].corr()
+plt.figure(figsize=(14,14))
+# annot=True显示每个方格的数据
+sns.heatmap(corr, annot=True)
+# plt.show()
+
 
 
 
