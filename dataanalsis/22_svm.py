9c0431a1231ae685559c2918c7630abacd50a38e
jayjiao <918718278@qq.com>
2021-06-22 19:46:26 +0800

    day 53

--- dataanalsis/22_svm.py
+++ dataanalsis/22_svm.py
@@ -3,10 +3,13 @@
 
 # 在sklearn中使用SVM算法
 # SVM做回归的时候，就可以使用SVR和LinearSVR
-from sklearn import svm
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv("../data/cancerdata.csv")
 # 数据探索
@@ -35,3 +38,31 @@ plt.figure(figsize=(14,14))
 # annot=True显示方格数据
 sns.heatmap(corr,annot=True)
 plt.show()
+
+# 特征选择
+
+# 特征选择
+features_remain = ['radius_mean','texture_mean', 'smoothness_mean','compactness_mean','symmetry_mean', 'fractal_dimension_mean'] 
+# 抽取30%的数据作为测试集，其余作为训练集
+# in this our main data is splitted into train and test
+train, test = train_test_split(data, test_size=0.3)
+# 抽取特征选择的数值作为训练和测试数据
+train_X = train[features_remain]
+train_y = train['diagnosis']
+test_X = test[features_remain]
+test_y = test['diagnosis']
+
+# 规范化数据到同一量级上
+# 采用Z-Score规范化数据，保证每个特征维度的数据均值为0，方差为1
+ss = StandardScaler()
+train_X = ss.fit_transform(train_X)
+test_X = ss.transform(test_X)
+# 创建SVM分类器
+model = svm.SVC()
+# 用训练集做训练
+model.fit(train_X, train_y)
+# 用测试集做预测
+prediction = model.predict(test_X)
+print("准确率:",metrics.accuracy_score(test_y,prediction))
+
+
