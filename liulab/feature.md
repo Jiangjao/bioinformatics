3a708213811850a43a0fbb140384b96c59d6c652
jayjiao <918718278@qq.com>
2022-10-18 15:06:33 +0800

    feature more

--- liulab/feature.md
+++ liulab/feature.md
@@ -1,4 +1,4 @@
-# emm
+# Feature description
 version 0.2.0, by xiaojiao
 <!-- 然后NAR文章的部分初稿我已经写好了，还是要细化所有的数据处理和功能的内容
 
@@ -6,7 +6,7 @@ version 0.2.0, by xiaojiao
 -   是，你参考他的架构就行，比如：他做的是代谢组学数据库优化，他提出了可能存在的问题，做了优化，一个优化他做的是一个数据库
 -   但是比如说，你提出茶叶数据库的问题，如何解决，然后你的体量是一个数据库 -->
 
-## feature
+## Feature
 1. Browse GWAS locus for single trait within given region. If not specified, the region with the **most significant P-value** was displayed. The scatter plot region can be easily changed by clicking a bar in the overview navigational Manhattan Plot panel. You could also search the GWAS data through our Jbrowser2.
 
 2. Web services:download data used by RESTful web-service
@@ -23,7 +23,7 @@ version 0.2.0, by xiaojiao
 >[`Metabolite signatures of diverse Camellia sinensis tea populations`](https://www.nature.com/articles/s41467-020-19441-1)
 
 
-## future feature
+## Future feature
 -   mQTL
     -   颜色
 
--- liulab/testProcess.py
+++ liulab/testProcess.py
@@ -1,5 +1,72 @@
 # !/usr/bin/python3
+# -- encoding = utf-8 ---
 # editor:xiaojiao 2022/10/18
 
 # Process pool & Process map 
 # preparation for loading data to database...
+# source from
+# https://blog.csdn.net/weixin_38819889/article/details/107815272
+
+
+# [python tutor]: process 、map progress bar : https://zhuanlan.zhihu.com/p/359369130
+import time
+from multiprocessing import Pool, Process
+import random, os
+
+def numsCheng(i):
+    return i * 2
+
+
+# 最重要的一点，多进程必须在 if __name__ == '__main__' 下写！
+# 否则会报错，当然了，你在函数里使用多进程，
+# 然后在 if __name__ == '__main__' 下调用这个函数也是允许的。
+def test1():
+    """
+        Suitable for CPU computing tasks
+    """
+    time1 = time.time()
+    # nums_list has to be iterable
+    nums_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    pool = Pool(processes=5)
+    result = pool.map(numsCheng, nums_list)
+    pool.close()
+    pool.join()
+
+    print(result)
+    time2 = time.time()
+    print("计算用时:", time2 - time1)
+
+def fun2(name):
+    print("Run task %s (%s)..." % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print("Task %s runs %0.2f seconds." % (name, (end - start)))
+
+def test2():
+    """
+        Process pool
+    """
+    pool = Pool(processes=5)
+
+    for i in range(15):
+        pool.apply_async(func=fun2, args=(i, ))
+
+    pool.close()
+    pool.join()
+    
+    print("end of test")
+
+if __name__ == "__main__":
+    # test1()
+    test2()
+
+
+
+
+
+
+
+
+
+
