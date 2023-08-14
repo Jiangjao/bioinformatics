7f9dbb9f965f2f6e19d01ec56d97dcd4121b414a
jayjiao <918718278@qq.com>
2023-08-14 17:02:02 +0800

    list_algos insert value into list && fix list_count bug

--- learn C the hard way/ex33/liblcthw/src/lcthw/dbg.h
+++ learn C the hard way/ex33/liblcthw/src/lcthw/dbg.h
@@ -1,39 +1,307 @@
-#ifndef __dbg_h__
-#define __dbg_h__
+#include "lcthw/list_algos.h"
+#include "lcthw/dbg.h"
 
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
+inline void ListNode_swap(ListNode *a, ListNode *b) {
+    check(a != NULL, "Invalid ListNode.");
+    check(b != NULL, "Invalid ListNode.");
+    void *temp = a->value;
+    a->value = b->value;
+    b->value = temp;
+error:
+    printf("something wrong with Node or NULL Node");
+}
 
-#ifdef NDEBUG
-#define debug(M, ...)
-#else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n",\
-        __FILE__, __LINE__, ##__VA_ARGS__)
-#endif
+int List_bubble_sort(List *list, List_compare cmp) {
+    int sorted = 1;
 
-#define clean_errno() (errno == 0 ? "None" : strerror(errno))
+    if (List_count(list) < 1) {
+        return 0;               // already sorted
+    }
 
-#define log_err(M, ...) fprintf(stderr,\
-        "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__,\
-        clean_errno(), ##__VA_ARGS__)
+    ListNode *cur = NULL;
+    do {
+        sorted = 1;
+        // LIST_FOREACH(list, first, next, cur) {
+        //     if (cur->next) {
+        //         if (cmp(cur->value, cur->next->value) > 0) {
+        //             ListNode_swap(cur, cur->next);
+        //             sorted = 0;
+        //         }
+        //     }
+        // }
+        cur = list->first;
+        while(cur->next) {
+            if (cmp(cur->value, cur->next->value) > 0) {
+                ListNode_swap(cur, cur->next);
+                sorted = 0;
+            }
+            cur = cur->next;
+        }
+    } while(!sorted);
 
-#define log_warn(M, ...) fprintf(stderr,\
-        "[WARN] (%s:%d: errno: %s) " M "\n",\
-        __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+    return 0;
+}
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n",\
-        __FILE__, __LINE__, ##__VA_ARGS__)
+// int List_bubble_sort(List *list, List_compare cmp) {
+//     if (List_count(list) < 1) {
+//         return 0;  // 已经排序好了
+//     }
 
-#define check(A, M, ...) if(!(A)) {\
-    log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+//     int sorted = 0;
+//     ListNode *cur = NULL;
+//     ListNode *last_unsorted = list->first;
+//     ListNode *last_swap = NULL;
 
-#define sentinel(M, ...)  { log_err(M, ##__VA_ARGS__);\
-    errno=0; goto error; }
+//     while (!sorted) {
+//         sorted = 1;
+//         cur = list->first;
+//         ListNode *prev = NULL; // 记录当前节点的前一个节点
+//         while (cur && cur->next != last_swap) {
+//             ListNode *next = cur->next; // 记录当前节点的下一个节点
+//             if (cmp(cur->value, next->value) > 0) {
+//                 // 直接交换节点指针
+//                 if (prev) {
+//                     prev->next = next;
+//                 } else {
+//                     list->first = next;
+//                 }
+//                 cur->next = next->next;
+//                 next->next = cur;
+//                 sorted = 0;
+//                 last_unsorted = cur;
+//             } else {
+//                 prev = cur;
+//                 cur = next;
+//             }
+//         }
+//         last_swap = last_unsorted;
+//     }
 
-#define check_mem(A) check((A), "Out of memory.")
+//     return 0;
+// }
 
-#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__);\
-    errno=0; goto error; }
+// int List_bubble_sort(List *list, List_compare cmp) {
+//     int length = List_count(list);
 
-#endif
+//     ListNode *node = list->first;
+//     int swapped = 0;
+//     for (int j = 0; j < length; j++) {
+        
+//         for (int i = 0; i < length - j; i ++) {
+//             if (cmp(node->value, node->next->value) > 0) {
+//                 ListNode_swap(node, node->next);
+//                 swapped = 1;
+//             }
+//             if (node->next) node = node->next;
+//         }
+//         if (swapped == 0) {
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+// inline List *List_merge(List *left, List *right, List_compare cmp) {
+//    List *result = List_create();
+   
+// 	void *val = NULL;
+// 	// int i = 0, j = 0;
+//    	while ((List_count(left) > 0) && (List_count(right) > 0)) {
+//     	if (cmp(List_first(left) , List_first(right)) <= 0) {
+//             // List_push(result, List_first(left));
+//             // i += 1;
+// 			val = List_shift(left);
+//         } else {
+//             // List_push(result, List_first(right));
+//             // j += 1;    
+// 			val = List_shift(right);        
+//         }
+// 		List_push(result, val);
+//    	}
+
+
+//     //  Add the remaining elements to the result array
+//     while (List_count(left) > 0) {
+//         // remove first node in left list
+//         val = List_shift(left);
+// 		List_push(result, val);
+//     }
+    
+//     // right as well
+//     while (List_count(right) > 0) {
+//         // remove first node in left list
+//         val = List_shift(right);
+// 		List_push(result, val);
+//     }
+
+//     return result;
+// }
+
+// https://github.com/preslavmihaylov/learn-c-the-hard-way/blob/master/33.LinkedListAlgorithms/liblcthw/src/lcthw/list_algos.c
+
+// List *List_merge_sort(List *array, List_compare cmp) {
+//     if (List_count(array) <= 1) {
+//         return array;
+//     }
+
+//     // Split the array
+//     // int mid = List_count(array) / 2;
+
+//     // List *left = List_create();
+//     // List *right = List_create();
+//     // LIST_FOREACH(array, first, next, cur) {
+//     //     if (mid > 0) {
+//     //         List_push(left, cur->value);
+//     //     } else {
+//     //         List_push(right, cur->value);
+//     //     }
+// 	// 	mid -= 1;
+//     // }
+//     List *left = List_split_by_mid(array);
+//     List *right = array;
+
+//     List *sort_left = List_merge_sort(left, cmp);
+//     List *sort_right = List_merge_sort(right, cmp);
+
+//     if (sort_left != left) List_destroy(left);
+//     if (sort_right != right) List_destroy(right);
+//     return List_merge(sort_left, sort_right, cmp);
+// }
+
+// https://github.com/preslavmihaylov/learn-c-the-hard-way/blob/master/33.LinkedListAlgorithms/liblcthw/src/lcthw/list_algos.c
+
+List *List_merge_sort(List *list, List_compare cmp) {
+    if (List_count(list) <= 1) return list;
+
+    List *left = List_create();
+    List *second = List_create();
+    List *result = List_create();
+
+    int i = 0;
+    LIST_FOREACH(list, first, next, cur) {
+
+        if (i < List_count(list) / 2) {
+            List_push(left, cur->value);
+        } else {
+            List_push(second, cur->value);
+        }
+
+        i++;
+    }
+
+    left = List_merge_sort(left, cmp);
+    second = List_merge_sort(second, cmp);
+	
+    ListNode *firstNode = left->first;
+    ListNode *secondNode = second->first;
+    while (firstNode != NULL && secondNode != NULL) {
+        if (cmp(firstNode->value, secondNode->value) <= 0) {
+            List_push(result, firstNode->value);
+            firstNode = firstNode->next;
+        } else {
+            List_push(result, secondNode->value);
+            secondNode = secondNode->next;
+        }
+    }
+
+    while (firstNode != NULL) {
+        List_push(result, firstNode->value);
+        firstNode = firstNode->next;
+    }
+    
+    while (secondNode != NULL){
+        List_push(result, secondNode->value);
+        secondNode = secondNode->next;
+    }
+	
+	//printList("RESULT ARRAY: ", result);
+    
+    List_destroy(left);
+    List_destroy(second);
+
+	return result;
+}
+
+// List *List_insert_sorted(List *list, void *value, List_compare cmp) {
+//     // list required sorted
+//     // check(is_sorted(list) == 0, "list is not sorted.");
+//     check(list != NULL, "Invalid list.");
+
+//     List *array = List_copy(list);
+//     List *result = NULL;
+
+//     ListNode *node = NULL;
+// 	node = array->first;
+
+//     if (List_count(list) == 0) {
+//         List_push(array, value);
+//         return array;
+//     } 
+
+//     if (cmp(node->value, value) >= 0) {
+//         List_unshift(array, value);
+//         return array;
+//     }
+
+//     if (cmp(array->last->value, value) <= 0) {
+//         List_push(array, value);
+//         return array;
+//     }
+
+//     // if the length of list less than 2
+
+//      if (List_count(list) <= 2) {
+//         // result = List_copy(list);
+//         result = array;
+//         List_push(result, value);
+//         List_bubble_sort(result, cmp);
+//         return result;
+//     }  
+
+//     while (node) {
+//         if ((cmp(node->value, value) <= 0) && (cmp(node->next->value, value) > 0)) {
+//             // split list into part two..
+//             ListNode *listafter = NULL;
+//             ListNode *listbefore = NULL;
+//             ListNode *newNode = calloc(1, sizeof(ListNode));
+//             check_mem(newNode);
+//             newNode->value = value;
+
+//             listafter = node->next;
+//             listbefore = node->next->prev;
+//             node->next = NULL;
+//             node->next->prev = NULL;
+
+//             // join two part 
+//             // List_push(listbefore, value);
+
+//             // maybe test this function List_cat... (xiaojiao 2023/08/14)
+
+//             // result = List_cat(listbefore, listafter);
+//             node->next = newNode;
+//             node->next->prev = newNode;
+//             newNode->prev = node;
+//             newNode->next = listafter;
+
+//             result = array;
+//             printf("result <<<<<<<<<<<<<<<<<<<<<%d>>>>>>>>>>>>>>>>>>>>>>>>>\n", List_count(result));
+//             result->count += 1;
+//         } 
+//         node = node->next;
+//     }
+//     result = array;
+
+//     return result;
+
+// error:
+//     return NULL;
+// }
+
+List *List_insert_sorted(List *list, void *value, List_compare cmp) {
+    List *result = List_copy(list);
+    List_push(result, value);
+    List_bubble_sort(result,  cmp);
+// error:
+    // return NULL;
+    return result;
+}
--- learn C the hard way/ex33/liblcthw/tests/list_algos_tests.c
+++ learn C the hard way/ex33/liblcthw/tests/list_algos_tests.c
@@ -2,10 +2,16 @@
 #include "lcthw/list_algos.h"
 #include <assert.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
 char *values[] = {"XXXX", "1234", "abcd", "xjvef", "NDSS"};
 
 #define NUM_VALUES 5
+#define RUN_TIMES 10000000
+
+typedef int (*List_compare)(const void *a, const void *b);
+
 
 List *create_words() {
     int i = 0;
@@ -71,42 +77,162 @@ char *test_merge_sort() {
     return NULL;
 }
 
+char *test_performance_of_merge_sort() {
+    List *words  = create_words();
+
+    // test 1000 times;
+    
+    // List *res = NULL;
+
+    // start $$ calculate time
+    clock_t start = clock();
+    for (int i = 0; i < RUN_TIMES; i++) {
+        List_merge_sort(words, (List_compare) strcmp);
+    }
+    clock_t end = clock();
+
+    // time spends
+    double tim_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("\nMerge Sort Time: %f seconds \n", tim_used);
+
+    List_destroy(words);
+    return NULL;
+}
+
+
+char *test_performance_of_bubble_sort() {
+    List *words  = create_words();
+
+    // test 1000 times;
+    
+    // List *res = NULL;
+
+    // start $$ calculate time
+    clock_t start = clock();
+    for (int i = 0; i < RUN_TIMES; i++) {
+        List_bubble_sort(words, (List_compare) strcmp);
+    }
+    clock_t end = clock();
+
+    // time spends
+    double tim_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("\nbubble_sort Time: %f seconds \n", tim_used);
+
+    List_destroy(words);
+    return NULL;
+}
+
+char *generate_random_string(int length) {
+    char *random_string = malloc((length + 1) * sizeof(char));
+    srand(time(NULL));
+
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int charset_length = sizeof(charset) - 1;
+
+    for (int i = 0; i < length; i++) {
+        int random_index = rand() % charset_length;
+        random_string[i] = charset[random_index];
+    }
+
+    random_string[length] = '\0';
+    return random_string;
+}
+
+
+List *create_list_by_specific_words(int word_length, int list_length) {
+    List *words = List_create();
+
+    // added into list
+    for (int j = 0; j < list_length; j++) {
+        char *value = generate_random_string(word_length);
+        List_push(words, value);
+    }
+    return words;    
+}
+
+void *create_different_size_of_bubble_sort(int list_length) {
+    List *words = create_list_by_specific_words(3, list_length);
+
+    // start $$ calculate time
+    clock_t start = clock();
+    for (int i = 0; i < RUN_TIMES; i++) {
+        List_bubble_sort(words, (List_compare) strcmp);
+    }
+    clock_t end = clock();
+
+    // time spends
+    double tim_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("\nbubble_sort Time: %f seconds \n", tim_used);
+
+    List_destroy(words);
+}
+
+void *create_different_size_of_merge_sort(int list_length) {
+    List *words = create_list_by_specific_words(3, list_length);
+
+    // start $$ calculate time
+    clock_t start = clock();
+    for (int i = 0; i < RUN_TIMES; i++) {
+        List_merge_sort(words, (List_compare) strcmp);
+    }
+    clock_t end = clock();
+
+    // time spends
+    double tim_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("\nmerge_sort Time: %f seconds \n", tim_used);
+
+    List_destroy(words);
+}
+
+char *test_different_size_of_bubble_sort() {
+    for (int i = 0; i < RUN_TIMES; i++) {
+        create_different_size_of_bubble_sort(i);
+    }
+    return NULL;
+}
+
+char *test_different_size_of_merge_sort() {
+    for (int i = 0; i < RUN_TIMES; i++) {
+        create_different_size_of_merge_sort(i);
+    }
+    return NULL;
+}
+
+char *test_list_insert_sorted() {
+    List *words  = create_words();
+
+    // should work on a list that needs sorting
+
+    List *res = List_merge_sort(words, (List_compare) strcmp);
+    mu_assert(is_sorted(res), "Should still be sorted after merge sort.");
+    mu_assert(List_count(res) == 5, "Wrong count after spliting list.");
+    List *res2 = List_insert_sorted(res, "2233", (List_compare) strcmp);
+    
+    mu_assert(is_sorted(res2), "Should still be sorted after inserted value.");
+	mu_assert(List_count(res) == 5, "Wrong count after insert list on source.");
+    mu_assert(List_count(res2) == 6, "Wrong count after insert list.");
+    List_destroy(res);
+
+    List_destroy(words);
+    return NULL;
+
+}
+
 char *all_tests() {
     mu_suite_start();
 
-    mu_run_test(test_bubble_sort);
-    mu_run_test(test_merge_sort);
+    // mu_run_test(test_bubble_sort);
+    // mu_run_test(test_merge_sort);
+    // mu_run_test(test_performance_of_merge_sort);
+    // mu_run_test(test_performance_of_bubble_sort);
 
+    // mu_run_test(test_different_size_of_merge_sort);
+    // test_list_insert_sorted
+    mu_run_test(test_list_insert_sorted);
     return NULL;
 }
 
 RUN_TESTS(all_tests);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
